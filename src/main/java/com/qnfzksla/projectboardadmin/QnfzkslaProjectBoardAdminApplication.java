55bf3f4b4c3f6a11f404d6e601c7016eb72a398a
qnfzksla <p971205@naver.com>
2023-03-31 12:58:07 +0900

    #4 - 스프링 부트 프로젝트 초기화

---
+++ src/main/java/com/qnfzksla/projectboardadmin/QnfzkslaProjectBoardAdminApplication.java
@@ -0,0 +1,15 @@
+package com.qnfzksla.projectboardadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+public class QnfzkslaProjectBoardAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnfzkslaProjectBoardAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qnfzksla/projectboardadmin/QnfzkslaProjectBoardAdminApplicationTests.java
@@ -0,0 +1,15 @@
+package com.qnfzksla.projectboardadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class QnfzkslaProjectBoardAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
