817c91e9c3eb1b53bc4429922e16bf7029beaeb9
qnfzksla <p971205@naver.com>
2023-04-11 04:08:49 +0900

    #14 - 비즈니스 로직 테스트, 커트롤러 테스트 작성
    게시물 목록, 단일 게시글, 게시글 삭제 기능을 만들 예정이고
    이들의 테스트를 작성함.

---
+++ src/main/java/com/qnfzksla/projectboardadmin/dto/ArticleDto.java
@@ -0,0 +1,24 @@
+package com.qnfzksla.projectboardadmin.dto;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record ArticleDto(
+
+        Long id,
+        UserAccountDto userAccount,
+
+        String title,
+
+        String content,
+        Set<String> hashtags,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+
+    public static ArticleDto of(Long id, UserAccountDto userAccount, String title,String content, Set<String> hashtags, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+      return new ArticleDto(id,userAccount,title, content ,hashtags,createdAt,createdBy,modifiedAt,modifiedBy);
+    }
+}
