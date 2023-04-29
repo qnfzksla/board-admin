497487efad1f79e2cb90645197be75582bee6207
qnfzksla <p971205@naver.com>
2023-04-29 16:47:56 +0900

    #28 - Admin Spring WebSpcket 예제를 횔용한 채팅 기능 구현

---
+++ src/main/java/com/qnfzksla/projectboardadmin/dto/websocket/WebSocketMessage.java
@@ -0,0 +1,8 @@
+package com.qnfzksla.projectboardadmin.dto.websocket;
+
+public record WebSocketMessage(String content) {
+
+    public static WebSocketMessage of(String content) {
+        return  new WebSocketMessage(content);
+    }
+}
